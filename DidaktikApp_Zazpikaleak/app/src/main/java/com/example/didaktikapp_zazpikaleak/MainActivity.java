89d175eb26b6e303534a3e14888bda3e8eb9eabc
EmmaVélez <emmavelgarci@hotmail.com>
2022-11-23 12:37:01 +0100

    Inicio con menu de desarrollador y zona 1

--- DidaktikApp_Zazpikaleak/app/src/main/java/com/example/didaktikapp_zazpikaleak/MainActivity.java
+++ DidaktikApp_Zazpikaleak/app/src/main/java/com/example/didaktikapp_zazpikaleak/MainActivity.java
@@ -6,36 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button play, salir;
+    TextView titulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        play = findViewById(R.id.botonMainJugar);
-        salir = findViewById(R.id.botonMainSalir);
+        titulo = findViewById(R.id.inicial_txt);
 
-        play.setOnClickListener(new View.OnClickListener() {
+        titulo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                System.out.println("iniciando juego...");
-                iniciarJuego();
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Desarrollador.class);
+                startActivity(i);
             }
         });
-
-        salir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
-
-    private void iniciarJuego(){
-        Intent i = new Intent(this, Zona1_3.class);
-        startActivity(i);
     }
 }
